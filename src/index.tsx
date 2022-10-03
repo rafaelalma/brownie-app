bb3a77bb104d78d479e4f0978b16a6e758e53d62
rafaelalma <rafaelalma92@gmail.com>
2022-10-03 19:41:20 +0200

    Add MUI CssBaseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
@@ -12,6 +14,7 @@ import '@fontsource/roboto/700.css'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
