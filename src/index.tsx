05f79678b5e1fc14d4bf97fc64cac84ca3d25c27
rafaelalma <rafaelalma92@gmail.com>
2022-10-03 19:34:23 +0200

    Install Roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
