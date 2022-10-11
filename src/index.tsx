6b02236297d18695944e2853ecb941da42766fd4
rafaelalma <rafaelalma92@gmail.com>
2022-10-11 20:14:39 +0200

    Change BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
@@ -26,9 +26,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 )
